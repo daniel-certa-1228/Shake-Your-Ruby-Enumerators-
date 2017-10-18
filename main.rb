d4f05cf89defdb06ff91731bcd80f94d7415d012
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-17 22:52:15 -0500

    done

---
+++ main.rb
@@ -0,0 +1,152 @@
+flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"]
+
+flowers_enum = flowers.each
+# puts flowers_enum.inspect
+
+puts flowers_enum.next
+puts flowers_enum.next
+puts flowers_enum.peek
+flowers_enum.rewind
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+flowers_enum.rewind
+puts flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+flowers_enum.next
+puts flowers_enum.next
+
